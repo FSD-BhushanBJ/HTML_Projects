89f9e166cc98617a1fc18958efdf5048daa32d3a
Bhushan Jatgade <bhushanjatgade@gmail.com>
2025-04-04 08:47:55 +0530

    Add files via upload

---
+++ bhushan-python-projects/input-class.py
@@ -0,0 +1,13 @@
+numberone = 10
+numbertwo = 20
+sum = numberone + numbertwo
+product = numberone * numbertwo
+
+
+
+#print(sum)
+print(numberone)
+
+print("Enter a number:")
+number = input()
+# print(number)
